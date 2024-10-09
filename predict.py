09ae3d864bb296fdf89ac01a286fb0d38ce73e2e
lamipaul <paulobest25@gmail.com>
2024-10-09 14:08:14 +0200

    manage single file as an input and better tqdm

--- predict.py
+++ predict.py
@@ -4,7 +4,7 @@ import torchcrepe, torch, librosa, soundfile
 import pandas as pd, numpy as np
 
 parser = argparse.ArgumentParser()
-parser.add_argument('indir', type=str, help="Directory with sound files to process")
+parser.add_argument('input', type=str, help="Directory with sound files to process, or a single file to process")
 parser.add_argument('--model_path', type=str, help="Path of model weights", default='model_all.pth')
 parser.add_argument('--compress', type=float, help="Compression factor used to shift frequencies into CREPE's range [32Hz; 2kHz]. \
     Frequencies are divided by the given factor by artificially changing the sampling rate (slowing down / speeding up the signal).", default=1)
@@ -21,25 +21,32 @@ model = torchcrepe.Crepe('full').eval().to(device)
 model.load_state_dict(torch.load(args.model_path, map_location=device))
 decoder = torchcrepe.decode.__dict__[args.decoder]
 
-files = [fn for fn in os.listdir(args.indir) if fn.split('.')[-1].upper() in soundfile._formats]
+if os.path.isdir(args.input):
+    files = [os.path.join(args.input, fn) for fn in os.listdir(args.input) if fn.split('.')[-1].upper() in soundfile._formats]
+elif os.path.isfile(args.input) and args.input.split('.')[-1].upper() in soundfile._formats:
+    files = [args.input]
 
-for filename in tqdm.tqdm(files):
+if len(files) == 0:
+    raise Exception(f"The given input argument {args.input} is either not a valid directory/file path, or it doesn't contain any sound file of the supported formats (please refer to pysoundfile documentation)")
+
+for ifile, filepath in enumerate(files):
     try:
-        sig, fs = librosa.load(os.path.join(args.indir, filename), sr=int(torchcrepe.SAMPLE_RATE * args.compress))
+        sig, fs = librosa.load(filepath, sr=int(torchcrepe.SAMPLE_RATE * args.compress))
     except:
-        print(f'Failed to load {filename}')
+        print(f'Failed to load {filepath}')
         continue
-    
+
     generator = torchcrepe.core.preprocess(torch.tensor(sig).unsqueeze(0), torchcrepe.SAMPLE_RATE, \
         hop_length=int(args.step / args.compress * torchcrepe.SAMPLE_RATE), batch_size=batch_size, device=device)
+    size = int(1 + (len(sig) - 1024) // (args.step * torchcrepe.SAMPLE_RATE))
     with torch.inference_mode():
-        preds = torch.vstack([model(frames).cpu() for frames in generator]).T.unsqueeze(0)
+        preds = torch.vstack([model(frames).cpu() for frames in tqdm.tqdm(generator, desc=f'{ifile}/{len(files)}: {filepath.split("/")[-1]}', total=size, leave=False)]).T.unsqueeze(0)
         f0 = (torchcrepe.core.postprocess(preds, decoder=decoder) * args.compress).squeeze()
     confidence = preds.max(axis=1)[0].squeeze()
-    time = np.arange(0, len(sig)/fs, args.step)
+    time = np.arange(0, len(sig)/fs + 1e-6, args.step)
     
     df = pd.DataFrame({'time':time, 'f0':f0, 'confidence':confidence})
-    df.to_csv(os.path.join(args.indir, filename.rsplit('.',1)[0]+'_f0.csv'), index=False)
+    df.to_csv(filepath.rsplit('.',1)[0]+'_f0.csv', index=False)
     # Plot F0 predictions over spectrograms
     if args.print:
         plt.figure(figsize=(max(6.4, 6.4*time[-1]/2), 4.8))
@@ -50,5 +57,5 @@ for filename in tqdm.tqdm(files):
         plt.ylim(0, f0[mask].max() * 1.5)
         plt.colorbar()
         plt.tight_layout()
-        plt.savefig(os.path.join(args.indir, filename.rsplit('.',1)[0])+'_f0.png')
+        plt.savefig(filepath.rsplit('.',1)[0]+'_f0.png')
         plt.close()
